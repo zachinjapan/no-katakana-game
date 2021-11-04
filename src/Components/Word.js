38effb99d087cc4791708756bf552bbd67a773f1
Zach Stone <zachinjapan888s@gmail.com>
2021-11-04 19:53:53 -0400

    change name of word changer button

--- src/Components/Word.js
+++ src/Components/Word.js
@@ -7,10 +7,11 @@ const Word = (props) => {
     const randomIndex = Math.floor(Math.random() * props.words.length);
     setCurrentWord(props.words[randomIndex]);
   };
+
   return (
     <div className="word-div">
       <h1>{currentWord}</h1>
-      <button onClick={currentWordHandler}> New Word</button>
+      <button onClick={currentWordHandler}>新しい単語</button>
     </div>
   );
 };
