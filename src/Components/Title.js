1b0c32cbc01123c1ef63974a923b8a1fe96857fc
Zach Stone <zachinjapan888@gmail.com>
2021-11-30 12:36:02 -0500

    fix typo

--- src/Components/Title.js
+++ src/Components/Title.js
@@ -3,7 +3,7 @@ import "../Components/Title.css";
 const Title = () => {
   return (
     <div className="title-div">
-      <h1 className="title"> ノーカタカタ </h1>
+      <h1 className="title"> ノーカタカナ </h1>
     </div>
   );
 };
